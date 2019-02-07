61c9d990e45397c3f388389dc06b2018b516d0f3
Katarzyna Węgrzyn <k8wegrzyn@gmail.com>
2019-02-07 09:11:54 +0100

    parameters comments

--- minimize.py
+++ minimize.py
@@ -4,8 +4,11 @@ import gzip
 import collections
 from Bio import SeqIO
 
-barcodes_file = "barcodes.fastq.gz", "barcodes2.fastq.gz"
-
+barcodes_file = "barcoded.fastq.gz", "barcodes2.fastq.gz"
+'''
+numpy uint16 - 2 bajty w array
+2^8 - 2^16 histogramy dla różnych modulo
+'''
 def get_minimizer(k, read):
     minimizer = 2**16
     for i in range(len(read) - k + 1):
@@ -15,9 +18,8 @@ def get_minimizer(k, read):
     return minimizer
 
 def barcode(record):
-   bc = record.description.split(' ')[1].split(':')[2] #return False if there is no barcode
-   if len(bc) == 18:
-      return bc
+   if "-1" in record.description:
+      return record.description.split(' ')[1].split(':')[2] 
    else:
       return False
 
@@ -28,23 +30,21 @@ def process_barcode(current_barcode, current_barcode_records, bc_min_dict):
 
 with gzip.open(barcodes_file[0], "rt") as file1, gzip.open(barcodes_file[1], "rt") as file2:
    bc_min_dict = collections.defaultdict(list) 
-   read_parsers=[SeqIO.parse(f, "fastq") for f in (file1, file2)]
+   read_parsers = [SeqIO.parse(f, "fastq") for f in (file1, file2)]
    current_records = [next(parser) for parser in read_parsers]
    while read_parsers:
-       print("current records: ", current_records)
-       current_barcode = min(barcode(record) for record in current_records if barcode(record)) #check if barcode exists
-       current_barcode_records = []
-       new_records = []
-       for parser,record in zip(read_parsers, current_records):
-           current_record = record
-           while barcode(current_record)==current_barcode:
-               current_barcode_records.append(current_record)
-               try:
-                   current_record=next(parser)
-               except StopIteration:
-                   read_parsers.remove(parser)
-                   break
-           else:
-               new_records.append(current_record)
-       current_records = new_records
-       process_barcode(current_barcode, current_barcode_records, bc_min_dict)
+      current_barcode = min(barcode(record) for record in current_records if barcode(record)) 
+      current_barcode_records = []
+      new_records = []
+      for parser, record in zip(read_parsers, current_records):
+         current_record = record
+         while barcode(current_record) == current_barcode:
+            current_barcode_records.append(current_record)
+            try:
+               current_record=next(parser)
+            except StopIteration:
+               read_parsers.remove(parser)
+               break
+         new_records.append(current_record)
+      current_records = new_records
+      d = process_barcode(current_barcode, current_barcode_records, bc_min_dict)
