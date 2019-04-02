892798faea51277dc87a4a76b3a5f9bb92138ca7
Katarzyna Węgrzyn <khaili@eduroam-10-21-6-128.mimuw.edu.pl>
2019-04-02 12:01:35 +0200

    next_valid undone

--- minimize.py
+++ minimize.py
@@ -12,14 +12,14 @@ startTime = datetime.now()
 
 #fastq.gz with barcodes location: ~/data/barcodes1/outs/barcoded.fastq.gz 1-8
 parser = argparse.ArgumentParser(description='Processing barcodes from fastq records', epilog="Used libraries: gzip, collections, Bio, numpy, argparse.")
-#parser.add_argument('infiles', metavar=('input'), help='input fastq.gz files with reads', nargs='+')
-parser.add_argument('barcodes_dir', metavar=('bc_dir'), help='path to directory with longranger results')
-parser.add_argument('barcoded_files', metavar=('bc_files'), help='relative path with barcoded files in "barcodesx" directory')
+parser.add_argument('infiles', metavar=('input'), help='input fastq.gz files with reads', nargs='+')
+#parser.add_argument('barcodes_dir', metavar=('bc_dir'), help='path to directory with longranger results')
+#parser.add_argument('barcoded_files', metavar=('bc_files'), help='relative path with barcoded files in "barcodesx" directory')
 parser.add_argument('--modulo', type=int, default=2**16, help='modulo value for minimizers creation (default: 2**16)')
 args = parser.parse_args()
 
 def get_minimizer(k, read):
-    minimizer = 2**16
+    minimizer = args.modulo
     for i in range(len(read) - k + 1):
         hashed_kmer = hash(read[i:i+k]) % args.modulo #problem with negative values
         if hashed_kmer <= minimizer: #rigthmost lowest value chosen
@@ -27,10 +27,14 @@ def get_minimizer(k, read):
     return minimizer
 
 def barcode(record):
-    if re.search("BX:Z:[ATCG]{16}-1", record.description):
-        return re.search("BX:Z:[ATCG]{16}-1", record.description).group()
-    else:
-        return "BX:Z:0"
+    #dodac jakies zabezpieczenie na pusty rekord
+    print("/////REC//////\n", record)
+    print(read_parsers)
+    if record:
+        if re.search("BX:Z:[ATCG]{16}-1", record.description):
+            return re.search("BX:Z:[ATCG]{16}-1", record.description).group()
+        else:
+            return "BX:Z:0"
 
 def process_barcode(current_barcode_records):
     minimizers = []
@@ -39,44 +43,54 @@ def process_barcode(current_barcode_records):
     minimizers.sort()
     return np.array(minimizers, dtype="uint16")
 
-#not used yet
 def next_valid(parser):
     try:
-        next(parser)
+        current_record = next(parser)
+        while barcode(current_record) == "BX:Z:0":
+                try:
+                    current_record = next(parser)
+                except StopIteration:
+                    read_parsers.remove(parser)
+                    return
     except StopIteration:
         read_parsers.remove(parser)
-    return
+        return
+    return current_record
 
 with ExitStack() as stack:
-    paths = [args.barcodes_dir + "barcodes" + str(x) + args.barcoded_files for x in range(1,9)]
-    files = [stack.enter_context(gzip.open(fname, "rt")) for fname in paths]
-    print(files)
+    #paths = [args.barcodes_dir + "barcodes" + str(x) + args.barcoded_files for x in range(1,9)]
+    #files = [stack.enter_context(gzip.open(fname, "rt")) for fname in paths]
+    files = [stack.enter_context(gzip.open(fname, "rt")) for fname in args.infiles]
     bc_min_dict = collections.defaultdict(list)
     read_parsers = [SeqIO.parse(f, "fastq") for f in files]
-    current_records = [next(parser) for parser in read_parsers]
+    #print(read_parsers)
+    #print([next_valid(parser) for parser in read_parsers])
+    #print([next_valid(parser) for parser in read_parsers])
+    current_records = [record for record in (next_valid(parser) for parser in read_parsers) if record is not None]
     with open("minimizers.tsv", "w") as out_mins, open("mins_per_bc.tsv", "w") as mbc:
         while read_parsers:
-            current_barcode = min(barcode(record) for record in current_records)
+            current_barcode = min(barcode(record) for record in current_records if record)
+            #print(current_records)
+            #print(current_barcode)
             current_barcode_records = []
             new_records = []
             for parser, record in zip(read_parsers, current_records):
                 current_record = record
+                #print(read_parsers)
+                #print("PARSER: ", parser)
+                #print(current_record)
+                print(current_records)
+                #print(current_barcode)
                 while barcode(current_record) == current_barcode:
                     current_barcode_records.append(current_record)
-                    try:
-                        current_record=next(parser)
-                    except StopIteration:
-                        read_parsers.remove(parser)
-                        break
+                    current_record = next_valid(parser)
                 new_records.append(current_record)
-            current_records = new_records
+                current_records = new_records
             mins = process_barcode(current_barcode_records)
-            if current_barcode != "BX:Z:0":
-                for m in mins:
-                    out_mins.write(str(m)+"\n")
-                mbc.write(current_barcode + "\t" + str(len(mins)) + "\n")
+            for m in mins:
+                out_mins.write(str(m)+"\n")
+            mbc.write(current_barcode + "\t" + str(len(mins)) + "\n")
 
 with open("summary.txt", "w+") as s:
-    s.write("Programm terminated succesfully.")
-    s.write("Time lapsed since begining: %s" % str(datetime.now() - startTime))
-
+    s.write("Programm terminated succesfully.\n")
+    s.write("Time lapsed since begining: %s\n" % str(datetime.now() - startTime))
