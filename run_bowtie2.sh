9a9a0c652e29b0b4721ad5e2a4f04ced603f8bfd
Katarzyna Wegrzyn <kwegrzyn@tiki.mimuw.edu.pl>
2019-05-13 09:23:25 +0000

    arguments passed correction

--- run_bowtie2.sh
+++ run_bowtie2.sh
@@ -4,8 +4,8 @@ for file in /home/kwegrzyn/data/grouped/*
 {
     bc=$(echo $file | tr "/" "." | tr "." "\n")
     set -- $bc
-    bowtie2 --no-unal -x genome -S "$5.sam" &
-    while  [ `ps -ef | grep bowtie2 | grep -v grep |wc -l` -gt 14 ]
+    bowtie2 --no-unal -x genome -U $file -S "/home/kwegrzyn/data/index/Homo_sapiens/UCSC/hg38/Sequence/Bowtie2Index/sams/$5.sam" &
+    while  [ `ps -ef | grep bowtie2 | grep -v grep |wc -l` -gt 1 ]
     do
         sleep 1;
     done
