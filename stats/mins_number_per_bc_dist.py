c2f74e3c08047c0bf87fc3acf467d8b64dd776f7
Katarzyna Węgrzyn <khaili@MacBook-Air-Katarzyna.local>
2019-11-03 13:35:59 +0100

    add minimizers count

--- stats/mins_number_per_bc_dist.py
+++ stats/mins_number_per_bc_dist.py
@@ -8,6 +8,8 @@ import seaborn as sns
 f = "/home/kwegrzyn/data/footprint_S_fixpvalue.tsv"
 
 footprint_data = pd.read_csv(f, sep="\t", names = ["barcode", "minimizers_group"], name="minimizers number per barcode in S footprint group")
-ax = sns.distplot(footprint_data, bins=20, kde=False)
+footprint_data['minimizers_group'] = footprint_data.minimiers_group.map(lambda x: [i.strip() for i in x.split(",")])
+footprint_data['mins_gorup_len'] = footprint_data.minimizers_group.apply(len)
+ax = sns.distplot(footprint_data['mins_gorup_len'], bins=20, kde=False)
 sns_plot = ax.get_figure()
 sns_plot.savefig("mins_number_per_bc_dist.pdf")
