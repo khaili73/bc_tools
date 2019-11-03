547cea1f041ff751677c9c8f8fa35ac4eaebe843
Katarzyna Wegrzyn <kwegrzyn@tiki.mimuw.edu.pl>
2019-11-03 14:52:02 +0000

    fix mins_number_per_bc

--- stats/mins_number_per_bc_dist.py
+++ stats/mins_number_per_bc_dist.py
@@ -7,9 +7,9 @@ import seaborn as sns
 
 f = "/home/kwegrzyn/data/footprint_S_fixpvalue.tsv"
 
-footprint_data = pd.read_csv(f, sep="\t", names = ["barcode", "minimizers_group"], name="minimizers number per barcode in S footprint group")
-footprint_data['minimizers_group'] = footprint_data.minimiers_group.map(lambda x: [i.strip() for i in x.split(",")])
-footprint_data['mins_gorup_len'] = footprint_data.minimizers_group.apply(len)
-ax = sns.distplot(footprint_data['mins_gorup_len'], bins=20, kde=False)
+footprint_data = pd.read_csv(f, sep="\t", usecols=[0,1,2], names = ["barcode", "footprint", "minimizers"])
+footprint_data['minimizers'] = footprint_data.minimizers.map(lambda x: [i for i in str(x).split(",")])
+footprint_data['minimizers_len'] = footprint_data.minimizers.apply(len)
+ax = sns.distplot(footprint_data['minimizers_len'], bins=range(1,31), kde=False)
 sns_plot = ax.get_figure()
-sns_plot.savefig("mins_number_per_bc_dist.pdf")
+sns_plot.savefig("S_mins_number_per_bc_dist_30bins.pdf")
