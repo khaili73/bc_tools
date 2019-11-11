40d90e5718ec57f1f4ebad08d23ef8afdff1cc72
Katarzyna Wegrzyn <kwegrzyn@tiki.mimuw.edu.pl>
2019-11-11 13:12:19 +0000

    upgrade violin plotting

--- stats/violin_plot_groups.py
+++ stats/violin_plot_groups.py
@@ -9,18 +9,21 @@ import seaborn as sns
 #local
 #f = "./groups_stats.tsv"
 #tiki
-f = "/home/kwegrzyn/data/groups_stats.tsv"
+f = "/home/kwegrzyn/data/groups/groups_stats.tsv"
 
-barcodes = pd.read_csv(f, sep="\t", names = ["barcode", "read_number", "groups", "valid_read_number"])
+barcodes = pd.read_csv(f, sep="\t", usecols=[0,1,2,3], names = ["barcode", "read_number", "groups", "valid_read_number"])
 #data.plot.scatter(x='groups', y='all_reads', c='DarkBlue')
-f, (ax) = plt.subplots(1, 1, figsize=(12, 4))
-f.suptitle('Clustering of barcodes reads mapping', fontsize=14)
-plt.xlabel('Groups')
-plt.ylabel('Number of reads')
+#f, (ax) = plt.subplots(1, 1, figsize=(12, 4))
+#f.suptitle('Clustering of barcodes reads mapping', fontsize=14)
+#plt.xlabel('Groups')
+#plt.ylabel('Number of reads')
 
-sns.violinplot(x="groups", y="read_number",
-               data=barcodes, inner="quart", linewidth=1.3, ax=ax)
-ax.set_xlabel("Number of groups",size = 12,alpha=0.8)
-ax.set_ylabel("Number of reads",size = 12,alpha=0.8)
+violin_plot = sns.violinplot(x="groups", y="read_number",
+               data=barcodes, inner="quart", linewidth=1.3)
+#ax.set_xlabel("Number of groups",size = 12,alpha=0.8)
+#ax.set_ylabel("Number of reads",size = 12,alpha=0.8)
 
-plt.savefig('/home/kwegrzyn/data/groups/sns_scatter_groups.pdf')
+violin_plot.set(ylim=(0,250),xlim=(0,6))
+fig = violin_plot.get_figure()
+fig.savefig('/home/kwegrzyn/data/groups/num_of_reads_per_group_violin_small.pdf')
+
