02d70b03b25e3e3606c601f159eafa552ad10dfd
Katarzyna Węgrzyn <khaili@MacBook-Air-Katarzyna.local>
2020-01-27 17:09:09 +0100

    debugged to test on small group

--- minimize_grouped.py
+++ minimize_grouped.py
@@ -5,22 +5,24 @@ from Bio.Seq import Seq
 from Bio.Alphabet import generic_dna
 from collections import namedtuple
 
-modulo = 24
+modulo = 2**24
 minimizers_values_file = 'all_mins.tsv'
 barcode_minimizers_file = 'bc_mins.tsv'
-grouped_path = '/home/kwegrzyn/data/grouped/'
+#grouped_path = '/home/kwegrzyn/data/grouped/'
+grouped_path = '/Users/khaili/mgr/bc_tools/grouped/'
 
 
 def minimize(k, sequence):
     dna_strand = Seq(sequence, generic_dna)
     rc_sequence = dna_strand.reverse_complement()
-    minimizer = abs(hash(str(sequence)[:3]))
+    minimizer = abs(hash(str(sequence)[:k])) % modulo
+    print("initial: ", minimizer)
     for seq in str(sequence), str(rc_sequence):
         for i in range(len(seq) - k + 1):
-            hashed_kmer = hash(seq[i:i + k])
-            if modulo:
-                hashed_kmer = hashed_kmer % modulo  # negative values
-            if abs(hashed_kmer) <= minimizer:  # choose rigthmost lowest value
+            print(seq[i:i + k])
+            hashed_kmer = hash(seq[i:i + k]) % modulo #NEGATIVE VALUES
+            print("hashed: ", hashed_kmer)
+            if abs(hashed_kmer) < minimizer:  # choose rigthmost lowest value
                 minimizer = abs(hashed_kmer)
     return minimizer
 
@@ -46,17 +48,17 @@ def get_barcode(record):
         return "BX:Z:0"
 
 
-with open(minimizers_values_file) as min_file, open(barcode_minimizers_file) as bc_file:
+with open(minimizers_values_file, 'w') as min_file, open(barcode_minimizers_file, 'w') as bc_file:
     directory = os.fsencode(grouped_path)
     for file in os.listdir(directory):
         filename = os.fsdecode(file)
         if filename.endswith(".fastq"):
-            with open(filename, 'r') as fastq:
+            with open(grouped_path+filename, 'r') as fastq:
                 records_parser = parse_fastq(fastq)
                 mins = []
                 for record in records_parser:
-                    min = minimize(record.sequence)
-                    mins.append(min)
-                    min_file.write(min)
-                bc = get_barcode(filename)
-                bc_file.write(bc, len(mins), mins)
+                    min = minimize(21, record.sequence)
+                    mins.append(str(min))
+                    min_file.write(str(min)+"\n")
+                    bc = get_barcode(record)
+                bc_file.write("\t".join((bc, str(len(mins)), str(",".join(mins))+"\n")))
